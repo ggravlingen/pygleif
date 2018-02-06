912c7fd478506f630dbdcd63201d990b41dd6a26
Patrik <slasktratt@gammelgravlingen.com>
2018-02-06 21:31:33 +0100

    Initial commit

---
+++ pygleif/gleif.py
@@ -0,0 +1,132 @@
+import urllib.request
+import json
+
+class GLEIF:
+    """Parse JSON from GLEIF registry. Supports v1 of API."""
+
+    def __init__(self, lei_code):
+        self.lei_code = lei_code
+
+    @property
+    def raw(self):
+        r = urllib.request.urlopen('https://leilookup.gleif.org/api/v1/leirecords?lei='+self.lei_code)
+        return json.loads(r.read().decode('UTF-8'))[0]
+
+    @property
+    def entity(self):
+        return GLEIFEntity(self)
+
+    @property
+    def lei(self):
+        return self.lei_data['LEI']
+
+    @property
+    def registration(self):
+        return GLEIFRegistration(self)
+
+    @property
+    def entity_business_register_entity_id(self):
+        return self.entity['BusinessRegisterEntityID']
+
+
+class GLEIFRegistration:
+
+    def __init__(self, registration):
+        self._registration = registration
+
+    @property
+    def raw(self):
+        return self._entity.raw['Registration']
+
+    @property
+    def initial_registration_date(self):
+        return self.raw['InitialRegistrationDate']['$']
+
+    @property
+    def last_update_date(self):
+        return self.raw['LastUpdateDate']['$']
+
+    @property
+    def managing_lou(self):
+        return self.raw['ManagingLOU']['$']
+
+    @property
+    def next_renewal_date(self):
+        return self.raw['NextRenewalDate']['$']
+
+    @property
+    def registration_status(self):
+        return self.raw['RegistrationStatus']['$']
+
+    @property
+    def validation_sources(self):
+        return self.raw['ValidationSources']['$']
+
+
+class GLEIFEntity:
+
+    def __init__(self, entity):
+        self._entity = entity
+
+    @property
+    def raw(self):
+        return self._entity.raw['Entity']
+
+    @property
+    def business_register_entity_id(self):
+        return self.raw['BusinessRegisterEntityID']['@register']
+
+    @property
+    def entity_status(self):
+        return self.raw['EntityStatus']['$']
+
+    @property
+    def legal_form(self):
+        return self.raw['LegalForm']['$']
+
+    @property
+    def legal_jurisdiction(self):
+        return self.raw['LegalJurisdiction']['$']
+
+    @property
+    def legal_name(self):
+        return self.raw['LegalName']['$']
+
+    @property
+    def headquarters_address(self):
+        return Address(self, 'HeadquartersAddress')
+
+    @property
+    def legal_address(self):
+        return Address(self, 'LegalAddress')
+
+
+class Address:
+
+    def __init__(self, address, address_type):
+        self._address = address
+        self.address_type = address_type
+
+    @property
+    def raw(self):
+        return self._address.raw[self.address_type]
+
+    @property
+    def city(self):
+        return self.raw['City']['$']
+
+    @property
+    def country(self):
+        return self.raw['Country']['$']
+
+    @property
+    def line1(self):
+        return self.raw['Line1']['$']
+
+    @property
+    def postal_code(self):
+        return self.raw['PostalCode']['$']
+
+    @property
+    def region(self):
+        return self.raw['Region']['$']
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+VERSION = "0.1"
+DOWNLOAD_URL = \
+    'https://github.com/ggravlingen/pygleif/archive/{}.zip'.format(VERSION)
+
+PACKAGES = find_packages(exclude=['tests', 'tests.*'])
+
+setup(
+  name='pygleif',
+  packages=PACKAGES,
+  python_requires='>=3.4',
+  version=VERSION,
+  description='API for LEI',
+  long_description=long_description,
+  author='ggravlingen',
+  author_email='no@email.com',
+  url='https://github.com/pygleif',
+  license='MIT',
+  keywords='lei-code lei api gleif',
+  download_url=DOWNLOAD_URL
+)
