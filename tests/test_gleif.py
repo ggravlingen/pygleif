41add4819136f89e4eac0db09d32682497c045fc
Patrik <slasktratt@gammelgravlingen.com>
2018-02-06 21:50:43 +0100

    Some fixes

--- tests/test_gleif.py
+++ tests/test_gleif.py
@@ -1,51 +1,24 @@
-from gleif.gleif import *
+from pygleif.gleif import GLEIFEntity
 
-LEI_DATA = '[{
-  "LEI": {"$": "549300MLUDYVRQOOXS22"},
-  "Entity": {"LegalName": {"$": "Nordic Credit Rating AS"},
-  "LegalAddress": {"Line1": {"$": "Haakon VII's gate 1"},
-        "City": {
-          "$": "Oslo"
-        },
-        "Region": {
-          "$": "NO-03"
-        },
-        "Country": {
-          "$": "NO"
-        },
-        "PostalCode": {
-          "$": "0161"
-        }
-      },
-      "HeadquartersAddress": {
-        "Line1": {
-          "$": "Haakon VII's gate 1"
-        },
-        "City": {
-          "$": "Oslo"
-        },
-        "Region": {
-          "$": "NO-03"
-        },
-        "Country": {
-          "$": "NO"
-        },
-        "PostalCode": {
-          "$": "0161"
-        }
-      },
-      "BusinessRegisterEntityID": {
-        "@register": "NO001"
-      },
-      "LegalJurisdiction": {
-        "$": "NO"
-      },
-      "LegalForm": {
-        "$": "AKSJESELSKAP"
-      },
-      "EntityStatus": {
-        "$": "ACTIVE"
-      }
+LEI_DATA = '''[{
+    "LEI": {"$": "549300MLUDYVRQOOXS22"},
+    "Entity": {"LegalName": {"$": "Nordic Credit Rating AS"},
+    "LegalAddress":{
+        "Line1": {"$": "Haakon VII's gate 1"},
+        "City": {"$": "Oslo"},
+        "Region": {"$": "NO-03"},
+        "Country": {"$": "NO"},
+        "PostalCode": {"$": "0161"}},
+    "HeadquartersAddress": {
+        "Line1": {"$": "Haakon VII's gate 1"},
+        "City": {"$": "Oslo"},
+        "Region": {"$": "NO-03"},
+        "Country": {"$": "NO"},
+        "PostalCode": {"$": "0161"}},
+      "BusinessRegisterEntityID": {"@register": "NO001"},
+      "LegalJurisdiction": {"$": "NO"},
+      "LegalForm": {"$": "AKSJESELSKAP"},
+      "EntityStatus": {"$": "ACTIVE"}
     },
     "Registration": {
       "InitialRegistrationDate": {
@@ -68,10 +41,10 @@ LEI_DATA = '[{
       }
     }
   }
-]'
+]'''
 
 
-def test_gleif:
-  data = GLEIFEntity(LEI_DATA['Entity'])
+def test_gleif():
+    data = GLEIFEntity(LEI_DATA['Entity'])
 
-  assert data.business_register_entity_id == 'NO001'
+    assert data.business_register_entity_id == 'NO001'
