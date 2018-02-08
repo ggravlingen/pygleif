e0bb85c48430a565c27a38f2e77ac2ae0ae7adc7
Patrik <slasktratt@gammelgravlingen.com>
2018-02-08 21:53:56 +0100

    Finalize const

--- pygleif/gleif.py
+++ pygleif/gleif.py
@@ -2,19 +2,32 @@ from .const import (
     ATTR_ADDRESS_CITY,
     ATTR_ADDRESS_COUNTRY,
     ATTR_DOLLAR_SIGN,
+    ATTR_ENTITY_BUSINESS_REGISTER_ENTITY_ID,
+    ATTR_ENTITY_ENTITY_STATUS,
+    ATTR_ENTITY_LEGAL_FORM,
+    ATTR_ENTITY_LEGAL_JURISDICTION,
+    ATTR_ENTITY_LEGAL_NAME,
     ATTR_ADDRESS_LINE1,
     ATTR_ADDRESS_POSTAL_CODE,
     ATTR_ADDRESS_REGION,
     ATTR_ADDRESS_TYPE_HQ,
     ATTR_ADDRESS_TYPE_LEGAL,
     ATTR_ENTITY,
+    ATTR_INITIAL_REGISTRATION_DATE,
+    ATTR_LAST_UPDATE_DATE,
+    ATTR_MANAGING_LOU,
+    ATTR_NEXT_RENEWAL_DATE,
+    ATTR_LEI,
     ATTR_REGISTER,
+    ATTR_REGISTRATION_STATUS,
     ATTR_REGISTRATION,
+    ATTR_VALIDATION_SOURCES,
     URL_API
 )
 import urllib.request
 import json
 from bs4 import BeautifulSoup
+
 
 class GLEIF:
     """Parse JSON from GLEIF registry. Supports v1 of API."""
@@ -29,7 +42,7 @@ class GLEIF:
 
     @property
     def lei(self):
-        return self.raw['LEI'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_LEI][ATTR_DOLLAR_SIGN]
 
     @property
     def entity(self):
@@ -51,31 +64,30 @@ class GLEIFRegistration:
 
     @property
     def initial_registration_date(self):
-        return self.raw['InitialRegistrationDate'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_INITIAL_REGISTRATION_DATE][ATTR_DOLLAR_SIGN]
 
     @property
     def last_update_date(self):
-        return self.raw['LastUpdateDate'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_LAST_UPDATE_DATE][ATTR_DOLLAR_SIGN]
 
     @property
     def managing_lou(self):
-        return self.raw['ManagingLOU'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_MANAGING_LOU][ATTR_DOLLAR_SIGN]
 
     @property
     def next_renewal_date(self):
-        return self.raw['NextRenewalDate'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_NEXT_RENEWAL_DATE][ATTR_DOLLAR_SIGN]
 
     @property
     def registration_status(self):
-        return self.raw['RegistrationStatus'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_REGISTRATION_STATUS][ATTR_DOLLAR_SIGN]
 
     @property
     def validation_sources(self):
-        return self.raw['ValidationSources'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_VALIDATION_SOURCES][ATTR_DOLLAR_SIGN]
 
 
 class GLEIFEntity:
-
     def __init__(self, entity):
         self._entity = entity
 
@@ -85,23 +97,23 @@ class GLEIFEntity:
 
     @property
     def business_register_entity_id(self):
-        return self.raw['BusinessRegisterEntityID'][ATTR_REGISTER]
+        return self.raw[ATTR_ENTITY_BUSINESS_REGISTER_ENTITY_ID][ATTR_REGISTER]
 
     @property
     def entity_status(self):
-        return self.raw['EntityStatus'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_ENTITY_ENTITY_STATUS][ATTR_DOLLAR_SIGN]
 
     @property
     def legal_form(self):
-        return self.raw['LegalForm'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_ENTITY_LEGAL_FORM][ATTR_DOLLAR_SIGN]
 
     @property
     def legal_jurisdiction(self):
-        return self.raw['LegalJurisdiction'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_ENTITY_LEGAL_JURISDICTION][ATTR_DOLLAR_SIGN]
 
     @property
     def legal_name(self):
-        return self.raw['LegalName'][ATTR_DOLLAR_SIGN]
+        return self.raw[ATTR_ENTITY_LEGAL_NAME][ATTR_DOLLAR_SIGN]
 
     @property
     def headquarters_address(self):
