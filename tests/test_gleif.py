693a4d3ee338bbe9b31b66b4c12df828c3dd8058
Patrik Lindgren <patrik@gammelgravlingen.com>
2018-04-02 16:40:27 +0200

    Fix lint

--- tests/test_gleif.py
+++ tests/test_gleif.py
@@ -14,25 +14,31 @@ class TestGLEIF(unittest.TestCase):
         self.assertEqual(self.data.lei, '549300MLUDYVRQOOXS22')
 
     def test_gleif_entity(self):
-        self.assertEqual(self.data.entity.business_register_entity_id, "NO001")
+        self.assertEqual(self.data.entity.business_register_entity_id,
+                         "NO001")
         self.assertEqual(self.data.entity.legal_jurisdiction, "NO")
         self.assertEqual(self.data.entity.legal_form, "Aksjeselskap")
-        self.assertEqual(self.data.entity.legal_name, "Nordic Credit Rating AS")
+        self.assertEqual(self.data.entity.legal_name,
+                         "Nordic Credit Rating AS")
         self.assertEqual(self.data.entity.entity_status, "ACTIVE")
 
     def test_gleif_entity_legal_address(self):
-        self.assertEqual(self.data.entity.legal_address.line1, "Haakon VII's gate 1")
+        self.assertEqual(self.data.entity.legal_address.line1,
+                         "Haakon VII's gate 1")
         self.assertEqual(self.data.entity.legal_address.city, "Oslo")
         self.assertEqual(self.data.entity.legal_address.region, "NO-03")
         self.assertEqual(self.data.entity.legal_address.country, "NO")
         self.assertEqual(self.data.entity.legal_address.postal_code, "0161")
 
     def test_gleif_entity_hq_address(self):
-        self.assertEqual(self.data.entity.headquarters_address.line1, "Haakon VII's gate 1")
+        self.assertEqual(self.data.entity.headquarters_address.line1,
+                         "Haakon VII's gate 1")
         self.assertEqual(self.data.entity.headquarters_address.city, "Oslo")
-        self.assertEqual(self.data.entity.headquarters_address.region, "NO-03")
+        self.assertEqual(self.data.entity.headquarters_address.region,
+                         "NO-03")
         self.assertEqual(self.data.entity.headquarters_address.country, "NO")
-        self.assertEqual(self.data.entity.headquarters_address.postal_code, "0161")
+        self.assertEqual(self.data.entity.headquarters_address.postal_code,
+                         "0161")
 
     def test_gleif_registration(self):
 
@@ -42,15 +48,19 @@ class TestGLEIF(unittest.TestCase):
                          datetime.datetime(2017, 4, 29, 2, 2, tzinfo=tzutc()))
         self.assertEqual(self.data.registration.registration_status, "ISSUED")
         self.assertEqual(self.data.registration.next_renewal_date,
-                         datetime.datetime(2018, 4, 27, 6, 32, tzinfo=tzutc()))
-        self.assertEqual(self.data.registration.managing_lou, "EVK05KS7XY1DEII3R011")
-        self.assertEqual(self.data.registration.validation_sources, "FULLY_CORROBORATED")
+                         datetime.datetime(2018, 4, 27, 6, 32,
+                                           tzinfo=tzutc()))
+        self.assertEqual(self.data.registration.managing_lou,
+                         "EVK05KS7XY1DEII3R011")
+        self.assertEqual(self.data.registration.validation_sources,
+                         "FULLY_CORROBORATED")
 
 
 class TestGLEIFRelationshipRecord(unittest.TestCase):
 
-    def test_relationship_record(self):
-        data = GLEIFParseRelationshipRecord(XML_DATA)
+    def setUp(self):
+        self.data = GLEIFParseRelationshipRecord(XML_DATA)
 
-        assert data.raw.Relationship.StartNode.NodeID.text\
-            ,  '097900BEJX0000001852'
+    def test_relationship_record(self):
+        self.assertEqual(self.data.raw.Relationship.StartNode.NodeID.text,
+                         '097900BEJX0000001852')
