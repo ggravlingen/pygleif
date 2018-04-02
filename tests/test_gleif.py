a37862d48f878834a53c438a138d1b588705d265
Patrik Lindgren <patrik@gammelgravlingen.com>
2018-04-02 16:37:50 +0200

    Refactor unit tests

--- tests/test_gleif.py
+++ tests/test_gleif.py
@@ -7,48 +7,44 @@ from sample_data import LEI_DATA, XML_DATA
 
 class TestGLEIF(unittest.TestCase):
 
+    def setUp(self):
+        self.data = GLEIF('549300MLUDYVRQOOXS22')
+
     def test_gleif(self):
-        data = GLEIF('549300MLUDYVRQOOXS22')
-
-        assert data.lei == '549300MLUDYVRQOOXS22'
-
+        self.assertEqual(self.data.lei, '549300MLUDYVRQOOXS22')
 
     def test_gleif_entity(self):
-        data = GLEIF('549300MLUDYVRQOOXS22')
+        self.assertEqual(self.data.entity.business_register_entity_id, "NO001")
+        self.assertEqual(self.data.entity.legal_jurisdiction, "NO")
+        self.assertEqual(self.data.entity.legal_form, "Aksjeselskap")
+        self.assertEqual(self.data.entity.legal_name, "Nordic Credit Rating AS")
+        self.assertEqual(self.data.entity.entity_status, "ACTIVE")
 
-        assert data.entity.business_register_entity_id == "NO001"
+    def test_gleif_entity_legal_address(self):
+        self.assertEqual(self.data.entity.legal_address.line1, "Haakon VII's gate 1")
+        self.assertEqual(self.data.entity.legal_address.city, "Oslo")
+        self.assertEqual(self.data.entity.legal_address.region, "NO-03")
+        self.assertEqual(self.data.entity.legal_address.country, "NO")
+        self.assertEqual(self.data.entity.legal_address.postal_code, "0161")
 
-        assert data.entity.legal_address.line1 == "Haakon VII's gate 1"
-        assert data.entity.legal_address.city == "Oslo"
-        assert data.entity.legal_address.region == "NO-03"
-        assert data.entity.legal_address.country == "NO"
-        assert data.entity.legal_address.postal_code == "0161"
-
-        assert data.entity.headquarters_address.line1 == "Haakon VII's gate 1"
-        assert data.entity.headquarters_address.city == "Oslo"
-        assert data.entity.headquarters_address.region == "NO-03"
-        assert data.entity.headquarters_address.country == "NO"
-        assert data.entity.headquarters_address.postal_code == "0161"
-
-        assert data.entity.business_register_entity_id == "NO001"
-        assert data.entity.legal_jurisdiction == "NO"
-        assert data.entity.legal_form == "Aksjeselskap"
-        assert data.entity.legal_name == "Nordic Credit Rating AS"
-        assert data.entity.entity_status == "ACTIVE"
-
+    def test_gleif_entity_hq_address(self):
+        self.assertEqual(self.data.entity.headquarters_address.line1, "Haakon VII's gate 1")
+        self.assertEqual(self.data.entity.headquarters_address.city, "Oslo")
+        self.assertEqual(self.data.entity.headquarters_address.region, "NO-03")
+        self.assertEqual(self.data.entity.headquarters_address.country, "NO")
+        self.assertEqual(self.data.entity.headquarters_address.postal_code, "0161")
 
     def test_gleif_registration(self):
-        data = GLEIF('549300MLUDYVRQOOXS22')
 
-        assert data.registration.initial_registration_date\
-            == datetime.datetime(2017, 4, 29, 2, 2, tzinfo=tzutc())
-        assert data.registration.last_update_date\
-            == datetime.datetime(2017, 4, 29, 2, 2, tzinfo=tzutc())
-        assert data.registration.registration_status == "ISSUED"
-        assert data.registration.next_renewal_date\
-            == datetime.datetime(2018, 4, 27, 6, 32, tzinfo=tzutc())
-        assert data.registration.managing_lou == "EVK05KS7XY1DEII3R011"
-        assert data.registration.validation_sources == "FULLY_CORROBORATED"
+        self.assertEqual(self.data.registration.initial_registration_date,
+                         datetime.datetime(2017, 4, 29, 2, 2, tzinfo=tzutc()))
+        self.assertEqual(self.data.registration.last_update_date,
+                         datetime.datetime(2017, 4, 29, 2, 2, tzinfo=tzutc()))
+        self.assertEqual(self.data.registration.registration_status, "ISSUED")
+        self.assertEqual(self.data.registration.next_renewal_date,
+                         datetime.datetime(2018, 4, 27, 6, 32, tzinfo=tzutc()))
+        self.assertEqual(self.data.registration.managing_lou, "EVK05KS7XY1DEII3R011")
+        self.assertEqual(self.data.registration.validation_sources, "FULLY_CORROBORATED")
 
 
 class TestGLEIFRelationshipRecord(unittest.TestCase):
@@ -57,4 +53,4 @@ class TestGLEIFRelationshipRecord(unittest.TestCase):
         data = GLEIFParseRelationshipRecord(XML_DATA)
 
         assert data.raw.Relationship.StartNode.NodeID.text\
-            == '097900BEJX0000001852'
+            ,  '097900BEJX0000001852'
