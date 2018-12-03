b9be5a78c4d48eafdef5ac5de1b577852367a4dd
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2018-12-03 14:12:30 +0100

    Add support for API v2.

--- pygleif/const.py
+++ pygleif/const.py
@@ -1,4 +1,4 @@
-URL_API = 'https://leilookup.gleif.org/api/v1/leirecords?lei='
+URL_API = 'https://leilookup.gleif.org/api/v2/leirecords?lei='
 URL_LEVEL2_CONCAT_FILES\
     = 'https://leidata.gleif.org/api/v1/concatenated-files/rr/%date%/zip'
 
@@ -6,9 +6,13 @@ ATTR_DOLLAR_SIGN = '$'
 
 ATTR_LEI = 'LEI'
 
-ATTR_ENTITY_BUSINESS_REGISTER_ENTITY_ID = 'BusinessRegisterEntityID'
+ATTR_ENTITY_REGISTRATION_AUTHORITY = 'RegistrationAuthority'
+ATTR_ENTITY_REGISTRATION_AUTHORITY_ENTITY_ID = 'RegistrationAuthorityEntityID'
+
+# RegistrationAuthorityEntityID
 ATTR_ENTITY_ENTITY_STATUS = 'EntityStatus'
 ATTR_ENTITY_LEGAL_FORM = 'LegalForm'
+ATTR_ENTITY_LEGAL_FORM_CODE = 'EntityLegalFormCode'
 ATTR_ENTITY_LEGAL_JURISDICTION = 'LegalJurisdiction'
 ATTR_ENTITY_LEGAL_NAME = 'LegalName'
 
@@ -23,7 +27,7 @@ ATTR_ADDRESS_TYPE_HQ = 'HeadquartersAddress'
 ATTR_ADDRESS_TYPE_LEGAL = 'LegalAddress'
 ATTR_ADDRESS_CITY = 'City'
 ATTR_ADDRESS_COUNTRY = 'Country'
-ATTR_ADDRESS_LINE1 = 'Line1'
+ATTR_ADDRESS_LINE1 = 'FirstAddressLine'
 ATTR_ADDRESS_POSTAL_CODE = 'PostalCode'
 ATTR_ADDRESS_REGION = 'Region'
 
