f39eeb72118786e2c7a9b9d74d2237f0c30cf6c4
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2022-03-15 20:53:59 +0100

    Cleanup (#22)

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
 # To use a consistent encoding
 from codecs import open
 from os import path
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -13,23 +13,22 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "2.0.0"
-DOWNLOAD_URL = \
-    'https://github.com/ggravlingen/pygleif/archive/{}.zip'.format(VERSION)
+VERSION = "3.0.0"
+DOWNLOAD_URL = 'https://github.com/ggravlingen/pygleif/archive/{}.zip'.format(VERSION)
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*'])
 
 setup(
-  name='pygleif',
-  packages=PACKAGES,
-  python_requires='>=3.8',
-  version=VERSION,
-  description='API for LEI',
-  long_description=long_description,
-  author='ggravlingen',
-  author_email='no@email.com',
-  url='https://github.com/ggravlingen/pygleif',
-  license='MIT',
-  keywords='lei-code lei api gleif leicode',
-  download_url=DOWNLOAD_URL
+    name='pygleif',
+    packages=PACKAGES,
+    python_requires='>=3.8',
+    version=VERSION,
+    description='API for LEI',
+    long_description=long_description,
+    author='ggravlingen',
+    author_email='no@email.com',
+    url='https://github.com/ggravlingen/pygleif',
+    license='MIT',
+    keywords='lei-code lei api gleif leicode',
+    download_url=DOWNLOAD_URL,
 )
