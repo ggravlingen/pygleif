66bdc891e3fafc1ff0b3cd19e0c458a526d42c9a
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2023-07-22 22:28:25 +0200

    Add Ruff (#199)

--- pygleif/api/__init__.py
+++ pygleif/api/__init__.py
@@ -1,5 +1,4 @@
 """Pydantic Representation of models."""
-from typing import List
 
 from pydantic import BaseModel, Field
 
@@ -18,4 +17,4 @@ class SearchResponse(BaseModel):
     """Represent search result response."""
 
     meta: Meta = Field(alias="meta")
-    data: List[Data] = Field(alias="data")
+    data: list[Data] = Field(alias="data")
