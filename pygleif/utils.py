50dbef3e70e32f70cd696dd27f2bc51c475a2d0b
“Patrik <21142447+ggravlingen@users.noreply.github.com>
2023-07-22 20:26:24 +0000

    Add ruff

--- pygleif/utils.py
+++ pygleif/utils.py
@@ -2,11 +2,11 @@
 from __future__ import annotations
 
 import json
-from typing import Any, Dict, List, Union, cast
+from typing import Any, cast
 from urllib import request
 
 
 def load_json(search_url: str, search_string: str) -> list[Any] | dict[Any, Any]:
     """Download data as JSON."""
     with request.urlopen(f"{search_url}{request.quote(search_string)}") as fdesc:
-        return cast(Union[Dict[Any, Any], List[Any]], json.loads(fdesc.read()))
+        return cast(dict[Any, Any] | list[Any], json.loads(fdesc.read()))
