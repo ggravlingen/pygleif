20db5c4d9031a9f0137051841af49cee3253ae18
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-07-06 20:34:16 +0200

    Refactor tests (#319)

--- pygleif/gleif.py
+++ pygleif/gleif.py
@@ -1,5 +1,7 @@
 """GLEIF API."""
 
+from typing import Any
+
 from .api import GLEIFResponse
 from .const import URL_API
 from .utils import load_json
@@ -10,5 +12,14 @@ class PyGleif:
 
     def __init__(self, lei_code: str) -> None:
         """Init class."""
-        json_data = load_json(search_url=URL_API, search_string=lei_code)
-        self.response = GLEIFResponse(**json_data)
+        self.lei_code = lei_code
+
+    @property
+    def json_response(self) -> list[Any] | dict[Any, Any]:
+        """Return JSON response."""
+        return load_json(search_url=URL_API, search_string=self.lei_code)
+
+    @property
+    def response(self) -> GLEIFResponse:
+        """Return response."""
+        return GLEIFResponse(**self.json_response)
