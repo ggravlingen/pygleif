9f1ef8aab95a742cc73ce3c6869e3a9f4b433f8c
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-09-08 10:17:02 +0000

    S

--- script/__init__.py
+++ script/__init__.py
@@ -1,13 +1,1 @@
 """Init."""
-from pygleif import PyGleif
-
-for entity in [
-    "549300MLUDYVRQOOXS22",
-    "M312WZV08Y7LYUC71685",
-    "IGJSJL3JD5P30I6NJZ34",
-    "3C7474T6CDKPR9K6YT90",
-]:
-    gleif: PyGleif = PyGleif(entity)
-    print(  # noqa: T201
-        gleif.response.data.attributes.registration.initial_registration_date,
-    )
