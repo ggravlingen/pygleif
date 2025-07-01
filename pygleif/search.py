3208d78d47321785e21f9c39f77f5512f0d0953d
Patrik <21142447+ggravlingen@users.noreply.github.com>
2025-07-01 17:25:36 +0000

    Introduce search_type argument

--- pygleif/search.py
+++ pygleif/search.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from enum import Enum
 from urllib import parse
 
 from pygleif.api import SearchResponse
@@ -9,13 +10,25 @@ from pygleif.api import SearchResponse
 from .base import PyGleifBase
 
 
+class SearchType(str, Enum):
+    """Enum for search types."""
+
+    FULL_TEXT = "fulltext"
+
+
 class Search(PyGleifBase):
     """Class to use the search form of the GLEIF web site."""
 
-    def __init__(self, search_string: str) -> None:
+    def __init__(
+        self,
+        search_string: str,
+        search_type: SearchType = SearchType.FULL_TEXT,
+    ) -> None:
         """Init class."""
         encoded_search_string = parse.quote(search_string, safe="")
-        self.search_string = f"?filter[fulltext]={encoded_search_string}"
+        self.search_string = (
+            f"?filter[{search_type.value.lower()}]={encoded_search_string}"
+        )
 
     @property
     def response(self) -> SearchResponse | None:
